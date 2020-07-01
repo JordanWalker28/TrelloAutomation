d92ad8a6fa7ab6cf52f9fc6c735e57a08f804488
Jordan Walker <jordanswalker28@gmail.com>
2020-07-01 21:11:08 +0100

    Added a GUI

--- program.py
+++ program.py
@@ -1,13 +1,9 @@
+
+from tkinter import *
 from trello import TrelloClient
 import requests
 
-api_key=''
-token=''
 
-client = TrelloClient(
-    api_key='',
-    token=''
-)
 
 def create_board(board_name):
     url = "https://api.trello.com/1/boards/"
@@ -23,15 +19,49 @@ def create_card(list_id, card_name):
     card_id = response.json()["id"]
     return card_id
 
+
+#last_board = all_boards[-1]
+#print(last_board.name)
+
+#print(all_boards[0].name)
+
+#lists = all_boards[0].all_lists()
+#print(lists)
+
+#all_boards[1].add_label("concept", "blue")
+
+#create_board("test")
+
+root = Tk()
+root.title("Tk dropdown example")
+
+# Add a grid
+mainframe = Frame(root)
+mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
+mainframe.columnconfigure(0, weight = 1)
+mainframe.rowconfigure(0, weight = 1)
+mainframe.pack(pady = 100, padx = 100)
+
+# Create a Tkinter variable
+tkvar = StringVar(root)
 all_boards = client.list_boards()
-last_board = all_boards[-1]
-print(last_board.name)
+list = []
+for i in all_boards:
+    list.append(i)
 
-print(all_boards[0].name)
+# Dictionary with options
+choices = list
+tkvar.set(all_boards[-1]) # set the default option
 
-lists = all_boards[0].all_lists()
-print(lists)
+popupMenu = OptionMenu(mainframe, tkvar, *choices)
+Label(mainframe, text="Choose a dish").grid(row = 1, column = 1)
+popupMenu.grid(row = 2, column =1)
 
-all_boards[1].add_label("concept", "blue")
+# on change dropdown value
+def change_dropdown(*args):
+    print( tkvar.get() )
 
-create_board("test")
+# link function to change dropdown
+tkvar.trace('w', change_dropdown)
+
+root.mainloop()
