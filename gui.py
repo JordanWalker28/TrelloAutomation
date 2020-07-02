7bbdd7a00808cbb591dfb7f71e591e01211d561d
JordanWalker28 <jordanswalker28@gmail.com@>
2020-07-02 23:53:35 +0100

    Added a text box and button

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 OptionList = [
 "Aries",
@@ -7,15 +8,31 @@ OptionList = [
 "Cancer"
 ] 
 
+def clickMe():
+    label.configure(text= 'Hello ' + name.get())
+
 app = tk.Tk()
 
-app.geometry('100x200')
+app.geometry('300x400')
 
 variable = tk.StringVar(app)
 variable.set(OptionList[0])
 
 opt = tk.OptionMenu(app, variable, *OptionList)
-opt.config(width=90, font=('Helvetica', 12))
+opt.config(width=28, font=('Helvetica', 12))
 opt.pack()
+opt.grid(column = 0, row = 0)
+
+label = ttk.Label(app, text = "Enter Your Name")
+label.grid(column = 0, row = 1)
+
+name = tk.StringVar()
+nameEntered = ttk.Entry(app, width = 15, textvariable = name)
+nameEntered.grid(column = 0, row = 2)
+ 
+ 
+button = ttk.Button(app, text = "Click Me", command = clickMe)
+button.grid(column= 0, row = 3)
+
 
 app.mainloop()
