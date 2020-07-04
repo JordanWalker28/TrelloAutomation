99fa77cec47e94b7e215763a2a67aec4399a9aba
Jordan Walker <jordan.walker@insightsoftware.com>
2020-07-04 23:24:38 +0100

    Made some changes

--- gui.py
+++ gui.py
@@ -22,14 +22,14 @@ variable = tk.StringVar(app)
 variable.set(OptionList[0])
 
 opt = tk.OptionMenu(app, variable, *OptionList)
-opt.config(width=28, font=('Helvetica', 12))
+opt.config(width=2, font=('Helvetica', 12))
 opt.grid(column = 0, row = 0)
 
 label = ttk.Label(app, text = "Enter Your Name")
 label.grid(column = 0, row = 1)
 
 name = tk.StringVar()
-nameEntered = ttk.Entry(app, width = 15, textvariable = name)
+nameEntered = ttk.Entry(app, width = 2, textvariable = name)
 nameEntered.grid(column = 0, row = 2)
  
  
@@ -40,6 +40,5 @@ textExample=tk.Text(app, height=10)
 textExample.grid(column=0, row =4)
 btnRead=tk.Button(app, height=1, text="Clear", command=clearTextInput)
 btnRead.grid(column=0, row =5)
-
 
 app.mainloop()
