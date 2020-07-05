1605b5fb60d557efa0320dc83c11a6b05e755929
Jordan Walker <jordanswalker28@gmail.com>
2020-07-05 23:40:30 +0100

    Added text label and button

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 
 from tkinter import *
+from tkinter import ttk
 from trello import TrelloClient
 import requests
 
@@ -18,7 +19,14 @@ def create_card(list_id, card_name):
     response = requests.request("POST", url, params=querystring)
     card_id = response.json()["id"]
     return card_id
-
+    
+# on change dropdown value
+def change_dropdown(*args):
+    print( tkvar.get() )
+    
+def clickMe():
+    print(nameEntered)
+    textExample.delete("1.0","end")
 
 #last_board = all_boards[-1]
 #print(last_board.name)
@@ -30,38 +38,51 @@ def create_card(list_id, card_name):
 
 #all_boards[1].add_label("concept", "blue")
 
-#create_board("test")
+#create_board("randomTest")
 
 root = Tk()
 root.title("Tk dropdown example")
-
+root.geometry('300x400')
 # Add a grid
 mainframe = Frame(root)
 mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
-mainframe.columnconfigure(0, weight = 1)
-mainframe.rowconfigure(0, weight = 1)
-mainframe.pack(pady = 100, padx = 100)
+mainframe.columnconfigure(0, weight = 2)
+mainframe.rowconfigure(0, weight = 2)
+mainframe.pack(padx = 0, pady = 0)
+
 
 # Create a Tkinter variable
 tkvar = StringVar(root)
-all_boards = client.list_boards()
-list = []
-for i in all_boards:
-    list.append(i)
 
+all_boards = client.list_boards()
+boardList = []
+
+for i in all_boards:
+    boardList.append(i)
+print(list)
+
+boardList = list(dict.fromkeys(boardList))
+print(boardList)
 # Dictionary with options
-choices = list
-tkvar.set(all_boards[-1]) # set the default option
+choices = boardList
+tkvar.set(choices[-1]) # set the default option
 
 popupMenu = OptionMenu(mainframe, tkvar, *choices)
-Label(mainframe, text="Choose a dish").grid(row = 1, column = 1)
-popupMenu.grid(row = 2, column =1)
-
-# on change dropdown value
-def change_dropdown(*args):
-    print( tkvar.get() )
+tkvar.trace('w', change_dropdown)
+Label(mainframe, text="Choose a Board:").grid(row = 0, column = 1)
+popupMenu.grid(row = 1, column =1)
 
 # link function to change dropdown
 tkvar.trace('w', change_dropdown)
 
+label = ttk.Label(mainframe, text = "Create Board:").grid(column = 0, row = 2)
+
+nameEntered = ttk.Entry(mainframe, width = 15).grid(column = 1, row = 2)
+print(nameEntered)
+
+button = ttk.Button(mainframe, text = "Click Me", command = clickMe).grid(column= 2, row = 2)
+
+
 root.mainloop()
+
+del boardList, tkvar, root
